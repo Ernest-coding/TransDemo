7f4e23da9db6d4677af37e996a3cbc7ef9cc543c
Ernest <ernest_work@163.com>
2022-07-25 15:33:34 +0800

    初次提交

---
+++ src/main/java/com/ernest/TransDemoApplication.java
@@ -0,0 +1,15 @@
+package com.ernest;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.ernest.mapper")
+public class TransDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransDemoApplication.class, args);
+    }
+
+}
