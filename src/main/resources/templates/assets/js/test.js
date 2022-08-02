243ec3b3a33589c72a700f1bda0246d67d4b71fa
EzioAuditoly <978058343@qq.com>
2022-08-02 18:37:19 +0800

    test

---
+++ src/main/resources/templates/assets/js/test.js
@@ -0,0 +1,9 @@
+var localurl="D:\\Learning_File\\Code_Program\\java\\TransDemo\\src\\main\\resources\\templates\\assets\\";
+var serverurl="/usr/local/duan/files/image/";
+$(function (){
+    setImg();
+})
+
+function setImg(){
+    $("#img01").src(localurl + "logo-5.png");
+}
