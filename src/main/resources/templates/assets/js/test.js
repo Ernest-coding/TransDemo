206d49aadf9f6c664bd0f358f46743eeea3c7954
EzioAuditoly <978058343@qq.com>
2022-08-03 16:27:26 +0800

    部分公司展示页面图片路径替换。

--- src/main/resources/templates/assets/js/test.js
+++ src/main/resources/templates/assets/js/test.js
@@ -6,6 +6,20 @@ $(function () {
  * /Path/ 映射到实际的路径就是 files/ ，所以直接在 /Path/ 后面接上 files 的子目录就行
  */
 function setImg() {
+    $("#about4").attr("src", "/Path/image/about/about4.jpg");
+    $("#about5").attr("src", "/Path/image/about/about5.jpg");
+    $("#about-01").attr("src", "/Path/image/about/about-01.jpg");
+    $("#about-02").attr("src", "/Path/image/about/about-02.jpg");
+    $("#about-03").attr("src", "/Path/image/about/about-03.jpg");
+    $("#about-img-1").attr("src", "/Path/image/about/about-img-1.jpg");
+    $("#office-img1").attr("src", "/Path/image/about/office-img1.jpg");
+    $("#office-img2").attr("src", "/Path/image/about/office-img2.jpg");
+
+
+
+
+
     $("#logo-4").attr("src", "/Path/image/logo/logo-4.png");
     $("#logo-5").attr("src", "/Path/image/logo/logo-5.png");
+
 }
